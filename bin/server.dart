62762162d4bfbdac599205efd6b4a945a154d270
iOS <lujie2012@163.com>
2021-04-19 11:52:56 +0800

    feat(cascades)

--- bin/server.dart
+++ bin/server.dart
@@ -1,34 +1,40 @@
-import 'dart:io';
-
-import 'package:args/args.dart';
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
 
-// For Google Cloud Run, set _hostname to '0.0.0.0'.
-const _hostname = 'localhost';
+const _hostname = '192.168.0.113';
+const _port = 8080;
 
 void main(List<String> args) async {
-  var parser = ArgParser()..addOption('port', abbr: 'p');
-  var result = parser.parse(args);
+  /// Cascades handles
+  final handlerCascade = shelf.Cascade().add((request) {
+    if (request.url.path == 'a') {
+      return shelf.Response.ok('handler a');
+    }
+    return shelf.Response.notFound('not found');
+  }).add((request) {
+    if (request.url.path == 'b') {
+      return shelf.Response.ok('handler b');
+    }
+    return shelf.Response.notFound('not found');
+  }).add((request) {
+    if (request.headers['My-Custom-Header'] != null) {
+      return shelf.Response.ok(request.headers['My-Custom-Header']);
+    }
+    return shelf.Response.notFound('not found');
+  }).handler;
 
-  // For Google Cloud Run, we respect the PORT environment variable
-  var portStr = result['port'] ?? Platform.environment['PORT'] ?? '8080';
-  var port = int.tryParse(portStr);
-
-  if (port == null) {
-    stdout.writeln('Could not parse port value "$portStr" into a number.');
-    // 64: command line usage error
-    exitCode = 64;
-    return;
-  }
-
-  var handler = const shelf.Pipeline()
+  /// Pipeline handlers
+  final handler = const shelf.Pipeline()
       .addMiddleware(shelf.logRequests())
-      .addHandler(_echoRequest);
+      .addMiddleware((innerHandler) => (request) async {
+            final updateRequest = request.change(
+              headers: {'My-Custom-Header': 'custom header value'},
+            );
+            return await innerHandler(updateRequest);
+          })
+      .addHandler(handlerCascade);
 
-  var server = await io.serve(handler, _hostname, port);
+  /// Create a shelf server
+  final server = await io.serve(handler, _hostname, _port);
   print('Serving at http://${server.address.host}:${server.port}');
 }
-
-shelf.Response _echoRequest(shelf.Request request) =>
-    shelf.Response.ok('Request for "${request.url}"');
